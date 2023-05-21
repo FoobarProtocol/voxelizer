d2127137e8b4503bca40d6df25c4934a56d1fe6c
Foobar Protocol <130879677+FoobarProtocol@users.noreply.github.com>
2023-05-21 15:36:42 -0400

    Add files via upload

---
+++ src/exporters/Exporter.js
@@ -0,0 +1,20 @@
+/**
+ * @author James Edwards <foobarprotocol@librehash.com>
+ */
+
+import { Volume } from "../volume";
+
+/**
+ * Class for exporting voxel data.
+ * @memberof module:voxelizer/exporters
+ */
+export class Exporter {
+
+  /**
+   * Export the volume data.
+   * @param {Volume} voxels 3D array with Voxels.
+	 * @param {function()} [onDone] Callback with exported data.
+   */
+  parse(volume, onDone) { }
+
+}
